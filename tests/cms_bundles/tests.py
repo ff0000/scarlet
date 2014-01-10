789aa8c8d29c450f8e76f98fa0124ebcf144de5b
markmiscavage <mark.miscavage@ff0000.com>
2014-01-10 09:45:56 -0800

    removed left in merge lines

--- tests/cms_bundles/tests.py
+++ tests/cms_bundles/tests.py
@@ -1,9 +1,12 @@
 import datetime
 import unittest
+import json
+from urllib import urlencode
 
 from django.test import TestCase
-from django.test.client import Client
+from django.test.client import Client, RequestFactory
 from django.contrib.auth.models import User
+
 
 from scarlet.cms import bundles, views
 from scarlet.cms.item import FormView
@@ -13,9 +16,8 @@ from models import *
 
 
 
-
-
 class BundleViewsTestCase(TestCase):
+
     def setup_test_user(self):
         user = User.objects.create_user('tester', 'tester@example.com', '1234')
         user.is_staff = True
@@ -80,6 +82,58 @@ class BundleViewsTestCase(TestCase):
     def test_delete(self):
         resp = self.client.get('/admin/blog/%s/edit/delete/' % self.post.pk)
         self.assertEqual(resp.status_code, 200)
+
+
+class MiscViewTestCase(TestCaseDeactivate):
+
+    def setup_test_user(self):
+        user = User.objects.create_user('tester', 'tester@example.com', '1234')
+        user.is_staff = True
+        user.save()
+        self.client.login(username='tester', password='1234')
+        self.user = user
+
+    def setUp(self):
+        self.client = Client()
+        self.setup_test_user()
+        self.cat1 = Category.objects.create(category='One')
+        self.cat2 = Category.objects.create(category='Two')
+        self.cat3 = Category.objects.create(category='Three')
+
+    def test_pagination(self):
+        #objects appear on pages in opposite order than they were added
+        nums = {1 : "Three", 2 : "Two", 3 : "One"}
+
+        for x in range (1, Category.objects.all().count()):
+            resp = self.client.get('/admin/blog/category/?page=%d' % x)
+            self.assertEqual(resp.status_code, 200)
+            for y in range(1, Category.objects.all().count()):
+                if (x == y):
+                    self.assertContains(resp, nums[x])
+                else:
+                    self.assertNotContains(resp, nums[y])
+
+    def test_listviewformsets(self):
+        resp = self.client.post('/admin/blog/category/', data = {'form-TOTAL_FORMS' : '1', 'form-INITIAL_FORMS' : '1',
+             'form-0-id' : self.cat1.pk, 'form-0-category' : "Uno"})
+        self.assertEqual(resp.status_code, 302)
+        c = Category.objects.filter(category = "Uno")
+        self.assertEqual(c.count(), 1)
+        self.assertEqual(Category.objects.filter(category = "One").count(), 0)
+
+        resp = self.client.post('/admin/blog/category/', data = {'form-TOTAL_FORMS' : '1', 'form-INITIAL_FORMS' : '1',
+             'form-0-id' : self.cat2.pk, 'form-0-category' : ""})
+        self.assertEqual(resp.status_code, 200)
+        self.assertEqual(Category.objects.filter(category = "").count(), 0)
+        self.assertEqual(Category.objects.filter(category = "Two").count(), 1)
+
+        resp = self.client.post('/admin/blog/category/?page=2', data = {'form-TOTAL_FORMS' : '1', 'form-INITIAL_FORMS' : '1',
+             'form-0-id' : self.cat2.pk, 'form-0-category' : "Dos"})
+        self.assertEqual(resp.status_code, 302)
+        self.assertEqual(Category.objects.filter(category="Dos").count(), 1)
+        self.assertEqual(Category.objects.filter(category="Two").count(), 0)
+
+
 
     def test_wrong_fields(self):
         f = FormView(model=Post)
