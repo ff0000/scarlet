0beef66754805a88457330a2b4227f99e66155f7
will mclafferty <will.mclafferty@ff0000.com>
2017-03-09 15:15:12 -0800

    yapf

--- tests/version_twomodels/tests.py
+++ tests/version_twomodels/tests.py
@@ -10,7 +10,7 @@ import models
 
 
 class ModelTests(TestCase):
-    fixtures = ('test_data.json',)
+    fixtures = ('test_data.json', )
 
     def _check_different_book_versions(self, bd, bp):
         self.assertTrue(bd.pk != bp.pk)
@@ -27,37 +27,32 @@ class ModelTests(TestCase):
         bd_gallery[0].name = 'changed'
         bd_gallery[0].save()
 
-        self.assertTrue(models.Review.objects.filter(book=bd.pk,
-                                                     text='changed').exists())
-        self.assertFalse(models.Review.objects.filter(book=bp.pk,
-                                                      text='changed').exists())
+        self.assertTrue(models.Review.objects.filter(book=bd.pk, text='changed').exists())
+        self.assertFalse(models.Review.objects.filter(book=bp.pk, text='changed').exists())
 
-        self.assertTrue(models.Gallery.objects.filter(book=bd.pk,
-                                                      name='changed').exists())
-        self.assertFalse(models.Gallery.objects.filter(book=bp.pk,
-                                                    name='changed').exists())
+        self.assertTrue(models.Gallery.objects.filter(book=bd.pk, name='changed').exists())
+        self.assertFalse(models.Gallery.objects.filter(book=bp.pk, name='changed').exists())
 
     def testBadModel(self):
         """
         need to define _base_model in Meta, its value should be a subclass
         of BaseModel
         """
-        Base = type('Base', (BaseModel,), {'__module__': __name__})
+        Base = type('Base', (BaseModel, ), {'__module__': __name__})
         # good
         Meta = type('Meta', (), {'_base_model': Base})
-        type('O1', (VersionModel,), {'Meta': Meta, '__module__': __name__})
+        type('O1', (VersionModel, ), {'Meta': Meta, '__module__': __name__})
 
         # bad
         try:
-            type('O2', (VersionModel,), {'Meta': Meta, '__module__': __name__})
+            type('O2', (VersionModel, ), {'Meta': Meta, '__module__': __name__})
         except:
             self.assertRaises(TypeError)
 
     def testNew(self):
         a = models.Author(name='straight')
         a.save()
-        self.assertTrue(models.AuthorBase.objects.filter(pk=a.object_id
-                                                         ).exists())
+        self.assertTrue(models.AuthorBase.objects.filter(pk=a.object_id).exists())
 
     def testGetVersion(self):
         a = models.AuthorBase.objects.get(pk=1)
@@ -104,18 +99,16 @@ class ModelTests(TestCase):
         base_book = models.BookBase.objects.get(pk=1)
         self.assertFalse(book.last_scheduled == base_book.v_last_save)
         self.assertTrue(models.Book.objects.filter(object_id=book.object_id,
-                         state=models.Book.SCHEDULED).exists())
+                                                   state=models.Book.SCHEDULED).exists())
         self.assertFalse(models.Book.objects.filter(object_id=book.object_id,
-                         state=models.Book.PUBLISHED).exists())
+                                                    state=models.Book.PUBLISHED).exists())
 
     def testPublishAuthor(self):
         author = models.Author.objects.get(name='test')
         author.publish()
 
-        ad = models.Author.objects.get(object_id=author.object_id,
-                                state=models.Author.DRAFT)
-        ap = models.Author.objects.get(object_id=author.object_id,
-                                state=models.Author.PUBLISHED)
+        ad = models.Author.objects.get(object_id=author.object_id, state=models.Author.DRAFT)
+        ap = models.Author.objects.get(object_id=author.object_id, state=models.Author.PUBLISHED)
 
         self.assertTrue(ad.pk != ap.pk)
 
@@ -127,18 +120,17 @@ class ModelTests(TestCase):
         ab = models.AuthorBase.objects.get(pk=1)
         ad.associates.add(ab)
         self.assertEqual(list(ap.associates.all()), [models.AuthorBase(pk=2)])
-        self.assertEqual(list(ad.associates.all()), [models.AuthorBase(pk=2),
-                                                     models.AuthorBase(pk=1)])
+        self.assertEqual(
+            list(ad.associates.all()),
+            [models.AuthorBase(pk=2), models.AuthorBase(pk=1)])
 
     def testPublishBook(self):
         book = models.Book.objects.get(pk=1)
 
         book.publish()
 
-        bd = models.Book.objects.get(object_id=book.object_id,
-                              state=models.Author.DRAFT)
-        bp = models.Book.objects.get(object_id=book.object_id,
-                              state=models.Author.PUBLISHED)
+        bd = models.Book.objects.get(object_id=book.object_id, state=models.Author.DRAFT)
+        bp = models.Book.objects.get(object_id=book.object_id, state=models.Author.PUBLISHED)
         self._check_different_book_versions(bd, bp)
 
     def testMakeDraftBook(self):
@@ -147,8 +139,7 @@ class ModelTests(TestCase):
         book.publish()
 
         # Change the draft
-        odb = models.BookBase.objects.get(pk=1
-                        ).get_version(state=models.Book.DRAFT)
+        odb = models.BookBase.objects.get(pk=1).get_version(state=models.Book.DRAFT)
         self.assertEqual(odb.last_scheduled, odb.last_save)
         odb.name = 'book2'
         odb.save()
@@ -159,14 +150,11 @@ class ModelTests(TestCase):
         self.assertTrue(odb.last_scheduled < odb.last_save)
 
         # Make the current published a draft
-        opb = models.BookBase.objects.get(pk=1
-                        ).get_version(state=models.Book.PUBLISHED)
+        opb = models.BookBase.objects.get(pk=1).get_version(state=models.Book.PUBLISHED)
         opb.make_draft()
 
-        db = models.BookBase.objects.get(pk=1
-                        ).get_version(state=models.Book.DRAFT)
-        pb = models.BookBase.objects.get(pk=1
-                        ).get_version(state=models.Book.PUBLISHED)
+        db = models.BookBase.objects.get(pk=1).get_version(state=models.Book.DRAFT)
+        pb = models.BookBase.objects.get(pk=1).get_version(state=models.Book.PUBLISHED)
 
         # Check everything changed, properly
         # new draft is the old published
@@ -192,8 +180,7 @@ class ModelTests(TestCase):
 
         book.publish()
 
-        bp = models.Book.objects.get(object_id=book.object_id,
-                                     state=models.Author.PUBLISHED)
+        bp = models.Book.objects.get(object_id=book.object_id, state=models.Author.PUBLISHED)
 
         self.assertEqual(models.Review.objects.all().count(), 4)
         self.assertEqual(models.Gallery.objects.all().count(), 4)
@@ -209,8 +196,7 @@ class ModelTests(TestCase):
     def testDeleteNotRelated(self):
         store = models.Store.objects.get(pk=1)
         store.publish()
-        sp = models.Store.objects.get(object_id=1,
-                                      state=models.Store.PUBLISHED)
+        sp = models.Store.objects.get(object_id=1, state=models.Store.PUBLISHED)
         self.assertEqual(list(sp.books.all()), [models.BookBase(pk=1)])
 
         # After deletion the book still exists
@@ -229,11 +215,11 @@ class ModelTests(TestCase):
 
         # should fail on second(duplicate) instance
         a2 = models.Author(name=a1.name)
-        with  self.assertRaises(ValidationError):
+        with self.assertRaises(ValidationError):
             a2.validate_unique()
 
         # multiple versions are not affected (can co-exists)
-        a3 = models.Author(object=a1.object,name=a1.name)
+        a3 = models.Author(object=a1.object, name=a1.name)
         a3.validate_unique()
 
     def testStatus1(self):
@@ -248,9 +234,8 @@ class ModelTests(TestCase):
 
         self.assertEqual(draft.last_save, draft.last_scheduled)
         self.assertEqual(draft.v_last_save, None)  # v_last_save empty
-        status = "%s: %s" % ('Publish Scheduled',
-                    formats.date_format(draft.date_published,
-                                        "SHORT_DATE_FORMAT"))
+        status = "%s: %s" % ('Publish Scheduled', formats.date_format(draft.date_published,
+                                                                      "SHORT_DATE_FORMAT"))
         self.assertEqual(status, draft.status_line())
 
         # publish it now. should get status as Published
@@ -261,9 +246,8 @@ class ModelTests(TestCase):
 
         self.assertEqual(draft2.last_save, draft2.last_scheduled)
         self.assertEqual(draft2.last_save, draft2.v_last_save)
-        status = "%s: %s" % ('Published',
-                    formats.date_format(draft2.date_published,
-                                        "SHORT_DATE_FORMAT"))
+        status = "%s: %s" % ('Published', formats.date_format(draft2.date_published,
+                                                              "SHORT_DATE_FORMAT"))
         self.assertEqual(status, draft2.status_line())
         self.assertEqual(draft2.date_published, when)
 
@@ -274,9 +258,8 @@ class ModelTests(TestCase):
         draft = models.Book.objects.get(vid=1)
         self.assertEqual(draft.last_save, draft.last_scheduled)
         self.assertTrue(draft.last_scheduled > draft.v_last_save)
-        status = "%s: %s" % ('Publish Scheduled',
-                    formats.date_format(draft.date_published,
-                                        "SHORT_DATE_FORMAT"))
+        status = "%s: %s" % ('Publish Scheduled', formats.date_format(draft.date_published,
+                                                                      "SHORT_DATE_FORMAT"))
         self.assertEqual(status, draft.status_line())
 
     def testStatus2(self):
@@ -286,14 +269,11 @@ class ModelTests(TestCase):
 
         book.publish()
 
-        book = models.Book.objects.get(object_id=book.object_id,
-                                       state=models.Book.PUBLISHED)
-        draft = models.Book.objects.get(vid=1,
-                                        state=models.Book.DRAFT)
+        book = models.Book.objects.get(object_id=book.object_id, state=models.Book.PUBLISHED)
+        draft = models.Book.objects.get(vid=1, state=models.Book.DRAFT)
         self.assertEqual(book.v_last_save, draft.v_last_save)
         self.assertEqual(draft.last_save, draft.last_scheduled)
         self.assertEqual(draft.last_save, draft.v_last_save)
-        status = "%s: %s" % ('Published',
-                    formats.date_format(draft.last_save,
-                                        "SHORT_DATE_FORMAT"))
+        status = "%s: %s" % ('Published', formats.date_format(draft.last_save,
+                                                              "SHORT_DATE_FORMAT"))
         self.assertEqual(status, book.status_line())
